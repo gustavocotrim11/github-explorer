15b9eb66d5d4708ff9d0e0938c8bb36f2bf3ff39
Gustavo Cotrim <gustavocotrim11@gmail.com>
2021-12-22 11:27:56 -0300

    Component state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { RepositoryList } from './components/RepositoryList';
+import { Counter } from './components/Counter';
 import './styles/global.scss';
 
 export function App() {
   return (
+    <>
     <RepositoryList />
+    <Counter />
+    </>
   );
 }
